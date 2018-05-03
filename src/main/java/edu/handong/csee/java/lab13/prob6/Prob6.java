0b67c2e74244a07d753c0a8e13d83ebc022c7c7f
Lee Jung Seok <21201072@handong.edu>
2018-05-03 22:46:42 +0900

    commit for Prob6

--- src/main/java/edu/handong/csee/java/lab13/prob6/Prob6.java
+++ src/main/java/edu/handong/csee/java/lab13/prob6/Prob6.java
@@ -30,21 +30,21 @@ class UpPoint implements CapitalPrintable { //Class UpPoint implements interface
 	
 	public String toString() //declare a toString() method
 	{
-		return "x : " + x + " y : " + y; //retrun this sentence
+		return "x : " + x + " y : " + y; //return this sentence
 	}
 }
 
-public class Prob6 {
+public class Prob6 { //declare a class
 	
-	public static void main(String[] args) { 
-		UpPoint p1 = new UpPoint(3,3);
-		DownPoint p2 = new DownPoint(2,5);
-		DownPoint p3 = new DownPoint(4,7);
-		UpPoint p4 = new UpPoint(9,12);
-		printer.print(p1);
-		printer.print(p2);
-		printer.print(p3);
-		printer.print(p4);
+	public static void main(String[] args) { //this is a main method
+		UpPoint p1 = new UpPoint(3,3); //instantiate UpPoint class
+		DownPoint p2 = new DownPoint(2,5); //instantiate DownPoint class
+		DownPoint p3 = new DownPoint(4,7); //instantiate DownPoint class
+		UpPoint p4 = new UpPoint(9,12); //instantiate UpPoint class
+		printer.print(p1); //print uppercase because p1 instance is instantiated from UpPoint class which implements CapitalPrintable interface
+		printer.print(p2); //print out the string that is not uppercase
+		printer.print(p3); //print out the string that is not uppercase
+		printer.print(p4); //print uppercase because p4 instance is instantiated from UpPoint class which implements CapitalPrintable interface
 	}
 }
 
